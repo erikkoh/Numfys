fabe097d3c9d4a027c801e893ebe2c66c99704e9
Kristoffer <e.k.ohlckers@gmail.com>
2025-01-23 10:51:40 +0100

    Made write to JSON function mutch more consistent with file pathing

--- Første_øvning/JSON_functions.jl
+++ Første_øvning/JSON_functions.jl
@@ -4,10 +4,21 @@ function write_to_JSON(bond_list,name, number_of_bonds)
     data_dic = Dict("Number of bonds" => number_of_bonds, "Bonds" => bond_list)
     json_data = JSON.json(data_dic)
     
-    file_path = "Første_øvning/JSON_files/" * name * ".json"
+    file_path = find_folder("JSON_files")* name * ".json"
+    println(file_path)
 
     open(file_path, "w") do file
         write(file, json_data)
     end
     println("Written to file path $file_path")
+end
+
+function find_folder(folder_name::String)
+    folder_path = ""
+    for (root, dirs, files) in walkdir(@__DIR__)
+        if occursin(folder_name, root)
+            return joinpath(root, folder_name)
+        end
+    end
+    error("Folder not found")
 end
