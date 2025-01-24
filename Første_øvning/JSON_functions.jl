a9706bf95bd50ae1e08a609521d41a0f14b0a27b
Kristoffer <e.k.ohlckers@gmail.com>
2025-01-24 20:21:42 +0100

    Rewrote find_folder function to give better consistency when finding folder

--- Første_øvning/JSON_functions.jl
+++ Første_øvning/JSON_functions.jl
@@ -1,15 +1,16 @@
 using JSON
 
-module JSON_functions
+module JSONFunctions
 
-export write_to_JSON, find_folder
+
+export find_folder, write_to_JSON
 
 function write_to_JSON(bond_list,name, number_of_bonds)
     data_dic = Dict("Number of bonds" => number_of_bonds, "Bonds" => bond_list)
     json_data = JSON.json(data_dic)
     
-    file_path = find_folder("JSON_files")* name * ".json"
-    println(file_path)
+    file_path = find_folder("JSON_files")*"/"* name * ".json"
+
 
     open(file_path, "w") do file
         write(file, json_data)
@@ -19,9 +20,10 @@ end
 
 function find_folder(folder_name::String)
     folder_path = ""
-    for (root, dirs, files) in walkdir(@__DIR__)
+    for (root, _dirs, _files) in walkdir(@__DIR__)
         if occursin(folder_name, root)
-            return joinpath(root, folder_name)
+            folder_path = root*"/"
+            return folder_path
         end
     end
     error("Folder not found")
