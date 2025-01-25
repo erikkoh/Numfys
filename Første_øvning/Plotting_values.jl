13e4ab1fbda2d335d212f86a7d858215e76dba75
Kristoffer <e.k.ohlckers@gmail.com>
2025-01-25 10:59:38 +0100

    Plotting values script to better visualize the critical probability

---
+++ Første_øvning/Plotting_values.jl
@@ -0,0 +1,16 @@
+using Plots
+
+include("Simulate_bonds.jl")
+
+
+simulation = simulate_bonds(1.0)
+
+p_0 = simulation[4]
+p_inf = simulation[5]
+s = simulation[8]
+println("Started plotting")
+p1 = plot(p_0, s, label = "<s>", xlabel = "p_0", ylabel = "s", title = "<s> as a function of p_0")
+p2 = plot(p_0, p_inf, label = "p_inf", xlabel = "p_0", ylabel = "p_inf", title = "p_inf as a function of p_0")
+plot(p1, p2, layout = (2,1))
+savefig(find_folder("Plots") * "values_plot.png")
+println("Finished plotting")
