acb8aef8fa7c7be3bb4c1a262ad3a6c6a44d04ce
Kristoffer <e.k.ohlckers@gmail.com>
2025-02-06 15:11:14 +0100

    Added avaraging of values

--- Første_øvning/Simulate_bonds.jl
+++ Første_øvning/Simulate_bonds.jl
@@ -1,6 +1,7 @@
 import JSON
 import Random
 using ProgressBars
+using Base.Threads
 include("JSON_functions.jl")
 
 using  .JSONFunctions: find_folder, write_to_JSON
@@ -10,12 +11,14 @@ Random.seed!(144)
 
 function swaping_bonds(bonds_list)
     println("Swaping bonds")
-    for i in ProgressBar(1:(length(bonds_list)-1))
-        candidate_number = rand(i+1:num_bonds)
+    println(length(bonds_list))
+    for i in (1:(length(bonds_list)-1))
+        candidate_number = rand(i+1:(length(bonds_list)))
         temp = bonds_list[i]
         bonds_list[i] = bonds_list[candidate_number]
         bonds_list[candidate_number] = temp
     end
+    println(length(bonds_list))
     println("Done!")
     return bonds_list
 end
@@ -50,6 +53,7 @@ function simulate_bonds(n::Int)
     bonds = JSON_info["Bonds"]
     num_bonds =JSON_info["Number of bonds"]
     num_nodes = bonds[end][1]
+    bonds = swaping_bonds(bonds)
     N = num_nodes
     sites = [-1 for i in 1:(N)]
     largest_cluster = Dict("index" => 1, "size" => 1)
@@ -63,7 +67,7 @@ function simulate_bonds(n::Int)
     s_step = (avarage_s-(N*p_inf[end])^2)/(N*(1-p_inf[end]))
     steps = 0
     println("Start loop")
-    for i in ProgressBar(1:num_bonds)
+    for i in (1:num_bonds)
         node1, node2 = bonds[i]
         root1 = find_root_node(node1,sites)
         root2 = find_root_node(node2,sites)
@@ -106,26 +110,99 @@ function simulate_bonds(n::Int)
     return largest_cluster, sites, largest_cluster_list, p_0, p_inf, p_inf_2, susept, s
 end
 
+function simulate_bonds_optimized(n::Int,iteration::Int)
+    JSON_info = JSON.parsefile(find_folder("JSON_files") * "Square_grid_bonds_$n.json")
+    bonds = JSON_info["Bonds"]
+    num_bonds =JSON_info["Number of bonds"]
+    num_nodes = Int64(bonds[end][1])
+    for _i in 1:iteration
+        bonds = swaping_bonds(bonds)
+    end
+    N = Int64(num_nodes)
+    sites = (fill(-1,N))
+    largest_cluster = Int64[1,-1]
+    number_of_activated_bonds = Int64(0)
+    p_0 = Float64[0.0]
+    p_inf = Float64[1/N]
+    p_inf_2 = Float64[(1/N)^2]
+    susept = Float64[N*(p_inf_2[1]- p_inf[1]^2)^(1/2)]
+    avarage_s = Float64(N)
+    s = Float64[0.0]
+    s_step = Float64(avarage_s-(N*p_inf[end])^2)/(N*(1-p_inf[end]))
+    steps = Int64(0)
+    println("Start loop")
+    for i in (1:num_bonds)
+        node1, node2 = bonds[i]
+        root1 = find_root_node(node1,sites)
+        root2 = find_root_node(node2,sites)
+        number_of_activated_bonds += 1
+        if root1 != root2
+            avarage_s = avarage_s -sites[root1]^2 - sites[root2]^2
+            if sites[root1] < sites[root2]
+                sites[root1] += sites[root2]
+                sites[root2] = root1
+                new_root = find_root_node(root1,sites)
+            else
+                sites[root2] += sites[root1]
+                sites[root1] = root2
+                new_root = find_root_node(root2,sites)
+            end
+            if sites[new_root] < largest_cluster[2]
+                largest_cluster = [new_root,sites[new_root]]
+            else
+                s_step = (avarage_s - (N * p_inf[end])^2) / (N * (1 - p_inf[end]))
+                s_step = isfinite(s_step) ? s_step : 0.0  # Ensure s_step is finite
+            end
+            avarage_s += sites[new_root]^2
+        end
+        push!(s,s_step)
+        push!(p_0, number_of_activated_bonds/num_bonds)
+        push!(p_inf,-largest_cluster[2]/N)
+        push!(p_inf_2,(largest_cluster[2]/N)^2)
+        push!(susept,N*(sum(p_inf_2)/length(p_inf_2) - (sum(p_inf)/length(p_inf))^2)^(1/2))
+        steps += 1
+    end
+    println("Number of steps: ", steps)
+    println("P_0: ", p_0[end])
+    println("Number of bonds: ", num_bonds)
+    println("Number of activated bonds: ", number_of_activated_bonds)
+    println("Susceptiblitly: ", susept[end])
+    println(length(s) == length(p_0)==length(p_inf)==length(p_inf_2)==length(susept))
+    println("Started looking for largest cluster:")
+    largest_cluster_list = [i for i in ProgressBar(eachindex(sites)) if find_root_node(i,sites) == largest_cluster[1]]
+    println("Done!")
+    return largest_cluster, sites, largest_cluster_list, p_0, p_inf, p_inf_2, susept, s
+end
+
+function testing_speed()
+    @time begin
+        simulate_bonds_optimized(300)
+    end
+    @time begin
+        simulate_bonds(300)
+    end
+end
+
+
 function p_images(p_list)
     p_dic = Dict{Float64,Any}()
     for p in p_list
         i = Int(floor(p*num_bonds))
-        largest_cluster, sites, largest_cluster_list, p_0 = simulate_bonds(i, bonds )
+        largest_cluster, sites, largest_cluster_list, p_0 = simulate_bonds(i)
         p_dic[p] = Dict("largest_cluster" => largest_cluster, "sites" => sites, "largest_cluster_list" => largest_cluster_list, "p_0" => p_0[end])
     end
     return p_dic
 end
 
 
-function avarage_values(iterations::Int64, bond_list)
+function avarage_values(iterations::Int64, n::Int64)
     p_inf_list = []
     susept_list = []
     s_list = []
     p_list = []
     p_inf_2_list = []
-    for i in ProgressBar(1:iterations)
-        bond_list = swaping_bonds(bond_list)
-        result = simulate_bonds(num_bonds, bond_list)
+    Threads.@threads for i in ProgressBar(1:iterations)
+        result = simulate_bonds_optimized(n, i)
         p = result[4]
         p_inf = result[5]
         p_inf_2 = result[6]
@@ -139,11 +216,13 @@ function avarage_values(iterations::Int64, bond_list)
         push!(s_list,s)
     end
 
+    println("started avraging values")
     p_inf_avarage = [sum([p[i] for p in p_inf_list ])/length(p_inf_list) for i in eachindex(p_inf_list[1])]
     susept_avarage = [sum([p[i] for p in susept_list ])/length(susept_list) for i in eachindex(susept_list[1])]
     s_avarage = [sum([p[i] for p in s_list ])/length(s_list) for i in eachindex(s_list[1])]
     p_avarage = [sum([p[i] for p in p_list])/length(p_list) for i in eachindex(p_list[1])]
     p_inf_2_avarage = [sum([p[i] for p in p_inf_2_list ])/length(p_inf_2_list) for i in eachindex(p_inf_2_list[1])]
     values_dic = Dict("p_inf" => p_inf_avarage,"p_inf_2" => p_inf_2_avarage, "susept" => susept_avarage, "s" => s_avarage, "p" => p_avarage)
-    write_to_JSON(values_dic,"avarage_values_$num_bonds")
+    println("Done!")
+    return values_dic
 end
