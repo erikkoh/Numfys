db138e10e613a85a1dd3f6d4af053a9a9d8a91e1
Kristoffer <e.k.ohlckers@gmail.com>
2025-03-11 21:45:16 +0100

    Implemented winding number algorithm

--- Assignment2/Checking_points.jl
+++ Assignment2/Checking_points.jl
@@ -16,7 +16,7 @@ function does_intersect(p1::Vector{Float64}, p2::Vector{Float64}, point::Vector{
     return false
 end
 
-function in_boundry(point::Vector{Float64}, boundry::Vector{Vector{Float64}})
+function in_boundary(point::Vector{Float64}, boundry::Vector{Vector{Float64}})
     count = 0
     list_length = length(boundry)
     if point in boundry
@@ -33,7 +33,33 @@ end
 
 
 
-function generate_grid(l::Int)
+function in_boundary_2(point::Vector{Float64}, boundary::Vector{Vector{Float64}})
+    count = 0
+    list_length = length(boundary)
+
+    function angle(a, b)
+        return acosd(clamp(a ⋅ b / (norm(a) * norm(b)), -1, 1))
+    end
+    function cross_product_sign(a, b)
+        return a[1] * b[2] - a[2] * b[1]
+    end
+    for i in 1:list_length
+        prev_point = boundary[i]
+        next_point = boundary[(i % list_length) + 1]
+        vec_prev = prev_point - point
+        vec_next = next_point - point
+        angle_val = angle(vec_next, vec_prev)
+        cross_sign = cross_product_sign(vec_prev, vec_next)
+        if cross_sign < 0
+            count -= angle_val
+        else
+            count += angle_val
+        end
+    end
+     return abs(count - 360) < 0.1 
+end
+
+function generate_grid(l::Int, checking_func = in_boundary)
     grid_size = max(4^l, 4^3)
     boundary = generate_square(l, grid_size)
     x_boundary = [point[1] for point in boundary]
@@ -43,23 +69,29 @@ function generate_grid(l::Int)
     grid_x = collect(x_min:x_max)
     grid_y = collect(y_min:y_max)
     grid = [[Float64(x), Float64(y)] for y in grid_y for x in grid_x]
-    grid_z = [in_boundry(Float64.(point), boundary) for point in ProgressBar(grid)]
+    grid_z = [checking_func(Float64.(point), boundary) for point in grid]
     grid = [[point[1], point[2], in_boundry] for (point, in_boundry) in zip(grid, grid_z)]
     return hcat(grid...), boundary
 end
 
-function checking_speed()
-    println("Checking speed")
-    @time begin
-        grid, _ = generate_grid(4)
-        println(size(grid))
-        println(typeof(grid))
-        println(grid[end,:])
+
+
+function comparing_speed()
+    for l in 1:4
+        time1 = @elapsed begin
+            generate_grid(l,in_boundary)
+        end
+        time2 = @elapsed begin
+            generate_grid(l,in_boundary_2)
+        end
+        println("Ray casting took $time1 and Winding number took $time2")
     end
 end
+
 function plotting_grid()
-    grid, _ = generate_grid(4)
-    z = grid[:,3]
+    grid, _ = generate_grid(3, in_boundary_2)
+    grid = convert(Array{Float64, 2}, grid)
+    z = grid[3,:]
     grid_size = Int(sqrt(length(z)))
     z_matrix = reshape(z, grid_size, grid_size)
     p1 = heatmap(z_matrix, xlabel="X-axis", ylabel="Y-axis", title="Grid")
@@ -90,6 +122,8 @@ function write_grid_to_file(l::Int)
         write(file, "boundary", boundary_matrix)
     end
 end
+
+comparing_speed()
 
 for i in 1:4
     println("started writing for l=$i")    
